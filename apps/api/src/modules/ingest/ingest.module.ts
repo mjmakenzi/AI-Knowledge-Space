f917d36c672a19e15fe79b0ba09f67a360f33091
m.mahdian <m.mahdian@arz.team>
2025-11-07 12:55:23 +0330

    add chunker anbd embeder

--- apps/api/src/modules/ingest/ingest.module.ts
+++ apps/api/src/modules/ingest/ingest.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DocumentEntity } from '../../entities/document.entity';
+import { IngestController } from './ingest.controller';
+import { IngestService } from './ingest.service';
+import { ExtractorService } from './extractor.service';
+import { ChunkEntity } from '../../entities/chunk.entity';
+import { EmbeddingEntity } from '../../entities/embedding.entity';
+import { EmbedderService } from './embedder.service';
+import { ChunkerService } from './chunker.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([DocumentEntity, ChunkEntity, EmbeddingEntity])],
+  controllers: [IngestController],
+  providers: [IngestService, ExtractorService, ChunkerService, EmbedderService],
+  exports: [IngestService],
+})
 export class IngestModule {}
